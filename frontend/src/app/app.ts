7f843994440e362456628dcccea2b28f7adceb80
Miroslav Ilic <miroslav.ilic@str8lines.com>
2025-11-10 16:04:52 +0100

    added navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Intro } from './components/shared/intro/intro';
+import { HeaderComponent } from './components/shared/header-component/header-component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Intro, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
+  private hasSeenIntro = signal(sessionStorage.getItem('intro-seen')==='true');
+  showIntro = signal(false);
+  fadeOutIntro = false
+
+  constructor() {
+    effect(()=>{
+      this.showIntro.set(!this.hasSeenIntro())
+    })
+
+    // window.showIntro.set(!this.hasSeenIntro());
+  }
+  hideIntro() {
+    this.fadeOutIntro = true;
+    setTimeout(()=>{
+      this.showIntro.set(false);
+      sessionStorage.setItem('intro-seen','true');
+      this.hasSeenIntro.set(true);
+    },500);
+
+    const introElement = document.querySelector('app-intro');
+    if (introElement) {
+      introElement.classList.add('fade-out');
+    }
+  }
+
 }
